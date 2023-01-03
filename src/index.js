c8dba0816aaf126cff22775d73dc3275e74d7ff3
Sky <44274227+smwoll@users.noreply.github.com>
2023-01-03 00:11:44 -0500

    update, store state

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ const cards = {
         "name": "The Magician",
         "desc": "Example description",
     },
+    3: {
+        "name": "The High Priestess",
+        "desc": "Example description",
+    },
 }
 
 let gameState = {
@@ -16,15 +20,15 @@ let gameState = {
     cardState: []
 }
 
-let availableCards = Object.keys(cards).filter((cardId) => {
-    return !gameState.cardState.find((card) => {
-        return card.index === cardId;
-    });
-});
+let availableCards = Object.keys(cards);
 
-console.log(availableCards );
-
-console.log('init game state', gameState);
+const updateAvailableCards = () => {
+    // Filter out any cards that have their index in the cardState.
+    const state = gameState.cardState;
+    const cardStateIndexes = state.map((card) => card.index.toString());
+    // Fitler out any cards that have been picked.
+    availableCards = availableCards.filter((card) => !cardStateIndexes.includes(card));
+}
 
 const pickRandomCard = () => {
     const numCards = availableCards.length;
@@ -44,4 +48,36 @@ const updateCardState = (index, team) => {
     });
 }
 
-console.log(pickRandomCard());
+updateCardState(3, 1);
+updateCardState(2, 1);
+
+
+// Store gamestate in the URL.
+const storeGameState = (gameState) => {
+    const encodedGameState = btoa(JSON.stringify(gameState));
+    const url = new URL(window.location.href);
+    url.searchParams.set('gameState', encodedGameState);
+    window.history.pushState({}, '', url);
+}
+
+const getGameState = () => {
+    const url = new URL(window.location.href);
+    const encodedGameState = url.searchParams.get('gameState');
+    if (encodedGameState) {
+        return JSON.parse(atob(encodedGameState));
+    }
+    return null;
+}
+
+const updateGameStateOnLoad = () => {
+    const newState = getGameState();
+    if (newState) {
+        console.log('game state loaded', gameState);
+        gameState = newState;
+        updateAvailableCards();
+    }
+}
+
+updateGameStateOnLoad();
+
+console.log('new pick', pickRandomCard());
